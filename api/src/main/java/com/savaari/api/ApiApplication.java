876b2fe9b914819b0dc0d67de22eacb835562999
Farjad Ilyas <ilyasfarjad@gmail.com>
2021-04-22 01:05:02 +0500

    Initial Commit for API with JDBC Support

---
+++ api/src/main/java/com/savaari/api/ApiApplication.java
@@ -0,0 +1,15 @@
+package com.savaari.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
