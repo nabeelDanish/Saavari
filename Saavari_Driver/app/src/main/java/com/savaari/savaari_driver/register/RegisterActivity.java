ac25299d0deba1ffdb94287a330dff0a8238c9c4
Nabeel Danish <nabeelben@gmail.com>
2021-04-22 10:35:32 +0500

    Register Activity and ViewModel

---
+++ Saavari_Driver/app/src/main/java/com/savaari/savaari_driver/register/RegisterActivity.java
@@ -0,0 +1,300 @@
+package com.savaari.savaari_driver.register;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NavUtils;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.savaari.savaari_driver.R;
+import com.savaari.savaari_driver.SavaariApplication;
+import com.savaari.savaari_driver.Util;
+import com.savaari.savaari_driver.entity.Driver;
+import com.savaari.savaari_driver.entity.Vehicle;
+import com.savaari.savaari_driver.register.fragments.FragmentClickListener;
+import com.savaari.savaari_driver.register.fragments.driver.DriverRegistrationFragment;
+import com.savaari.savaari_driver.register.fragments.menu.VehicleMenuFragment;
+import com.savaari.savaari_driver.register.fragments.vehicle.VehicleRegistrationFragment;
+import com.savaari.savaari_driver.ride.RideActivity;
+import com.savaari.savaari_driver.settings.SettingsActivity;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public class RegisterActivity
+        extends Util
+        implements FragmentClickListener, NavigationView.OnNavigationItemSelectedListener
+{
+    private final String LOG_TAG = this.getClass().getCanonicalName();
+
+    // Main Attributes
+
+    // Data and flags
+    private RegisterViewModel registerViewModel;
+    private ScheduledFuture<?> future = null;
+    private boolean networkConnection = false;
+
+    // UI
+    private ProgressBar loadingCircle;
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private View headerView;
+    private TextView navUsername;
+    private TextView navEmail;
+    private Toolbar myToolbar;
+
+
+    // ------------------------------------------------------------------------------------------
+    //                                      MAIN METHODS
+    // ------------------------------------------------------------------------------------------
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        themeSelect(this);
+        super.onCreate(savedInstanceState);
+
+        // Setting UI Elements
+        setContentView(R.layout.activity_register);
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        headerView = navigationView.getHeaderView(0);
+        navUsername = headerView.findViewById(R.id.header_nickname);
+        navEmail = headerView.findViewById(R.id.header_email);
+        myToolbar = findViewById(R.id.toolbar);
+        myToolbar.setTitle("Settings");
+        setSupportActionBar(myToolbar);
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        this.getSupportActionBar().setDisplayShowHomeEnabled(true);
+        // loadingCircle = findViewById(R.id.progressBar_2);
+
+        // Getting Stored Data
+        Intent recvIntent = getIntent();
+        networkConnection = recvIntent.getBooleanExtra("API_CONNECTION", false);
+        if (!networkConnection) {
+            Toast.makeText(this, "No network connection", Toast.LENGTH_SHORT).show();
+        }
+        int USER_ID = recvIntent.getIntExtra("USER_ID", -1);
+
+        // Creating View Model
+        registerViewModel = ViewModelProviders.of(this, new RegisterViewModelFactory(
+                ((SavaariApplication) this.getApplication()).getRepository())
+        ).get(RegisterViewModel.class);
+
+        // Calling the Service again to Load Driver if not already
+        if (registerViewModel.getDriver() == null) {
+            future = ((SavaariApplication) getApplication()).scheduledThreadPoolExecutor.scheduleWithFixedDelay(() -> registerViewModel.persistConnection(USER_ID),
+                    0L, 8L, TimeUnit.SECONDS);
+        }
+
+        // On Persist Connection
+        registerViewModel.getPersistConnection().observe(this, aBoolean -> {
+            if (aBoolean != null) {
+                if (aBoolean) {
+                    Toast.makeText(this, "Connection Established!", Toast.LENGTH_SHORT).show();
+                    registerViewModel.loadUserData(USER_ID);
+                } else {
+                    Toast.makeText(this, "Connection Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // On Data Loaded
+        registerViewModel.getUserdataLoaded().observe(this, aBoolean -> {
+            if (aBoolean != null) {
+                // loadingCircle.setVisibility(View.INVISIBLE);
+                if (aBoolean) {
+                    Toast.makeText(this, "User Data Loaded!", Toast.LENGTH_SHORT).show();
+                    if (future != null) {
+                        future.cancel(true);
+                    }
+                    checkDriverStatus();
+                } else {
+                    Toast.makeText(this, "User Data could not be loaded!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    } // End of OnCreate Function()
+
+    // Main Function for driver account logic
+    private void checkDriverStatus()
+    {
+        // Some UI Stuff
+        navUsername.setText(registerViewModel.getDriver().getUsername());
+        navEmail.setText(registerViewModel.getDriver().getEmailAddress());
+
+        Intent i = getIntent();
+        boolean fromRideActivity = i.getBooleanExtra("FROM_RIDE", false);
+        int where = i.getIntExtra("WHERE", -1);
+        // Main Check depending on Driver Account Status
+        switch (registerViewModel.getDriver().getStatus())
+        {
+            case Driver.DV_DEFAULT:
+            {
+                // Driver needs to fill the form and send it
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.register_frame, DriverRegistrationFragment.newInstance(this))
+                        .commit();
+                break;
+            }
+            case Driver.DV_REQ_SENT:
+            {
+                // Request is sent, Driver can add vehicles or see vehicles status
+                launchVehicleMenuFragment();
+            }
+            case Driver.DV_REQ_REJECTED:
+            {
+                // Request Rejected: handle later
+                break;
+            }
+            case Driver.DV_REQ_APPROVED:
+            {
+                // Request Approved, check vehicles
+                if (fromRideActivity)
+                {
+                    switch (where)
+                    {
+                        case 1:
+                            launchDocumentFragment();
+                            break;
+                        case 2:
+                            launchVehicleMenuFragment();
+                            break;
+                    }
+                }
+                else
+                {
+                    if (registerViewModel.getDriver().getActiveVehicle().getVehicleID() != Vehicle.DEFAULT_ID) {
+                        // Goto Ride Activity
+                        launchRideActivity();
+                    } else {
+                        launchVehicleMenuFragment();
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    private void launchVehicleMenuFragment() {
+        myToolbar.setTitle("VEHICLES");
+        Log.d(LOG_TAG, "launchVehicleMenuFragment: launching Vehicle Menu Fragment");
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.register_frame, VehicleMenuFragment.newInstance(this))
+                .commit();
+    }
+    private void launchDocumentFragment() {
+        myToolbar.setTitle("DOCUMENTS");
+        // Driver needs to fill the form and send it
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.register_frame, DriverRegistrationFragment.newInstance(this))
+                .commit();
+    }
+
+    private void launchRideActivity() {
+        Log.d(LOG_TAG, "launchRideActivity: Launching Ride Activity");
+        Intent i = new Intent(com.savaari.savaari_driver.register.RegisterActivity.this, RideActivity.class);
+        i.putExtra("API_CONNECTION", true);
+        i.putExtra("USER_ID", registerViewModel.getDriver().getUserID());
+        startActivity(i);
+        finish();
+    }
+
+    // Interface Implementation
+    @Override
+    public void onDriverRegistrationClick() {
+        // Driver needs to fill the form and send it
+        myToolbar.setTitle("DOCUMENT");
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.register_frame, DriverRegistrationFragment.newInstance(this))
+                .commit();
+    }
+
+    @Override
+    public void onVehicleRegistrationClick() {
+        myToolbar.setTitle("VEHICLES");
+        // Driver needs to fill the form and send it
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.register_frame, VehicleRegistrationFragment.newInstance(this))
+                .commit();
+    }
+
+    @Override
+    public void onVehicleRegistrationClick(int position) {
+        VehicleRegistrationFragment fragment = VehicleRegistrationFragment.newInstance(this);
+        myToolbar.setTitle("VEHICLES");
+        Bundle bundle = new Bundle();
+        bundle.putInt("POSITION", position);
+        fragment.setArguments(bundle);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.register_frame, fragment)
+                .commit();
+    }
+
+    @Override
+    public void onBackToRideClick() {
+        // Going back to Ride Activity
+        launchRideActivity();
+    }
+
+    @Override
+    public void onVehicleMenuClick() {
+        launchVehicleMenuFragment();
+    }
+
+    // OnNavigationItemSelected Listener from Navigation Bar Click
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        drawer.closeDrawer(GravityCompat.START);
+        Intent i;
+        // Main Switch Case for NavBar
+        switch (item.getItemId()) {
+            case (R.id.nav_your_trips):
+            case (R.id.nav_help):
+            case (R.id.nav_wallet):
+                break;
+            case (R.id.nav_your_documents):
+                launchDocumentFragment();
+                break;
+            case (R.id.nav_your_vehicles):
+                launchVehicleMenuFragment();
+                break;
+            case (R.id.nav_settings):
+                i = new Intent(com.savaari.savaari_driver.register.RegisterActivity.this, SettingsActivity.class);
+                startActivity(i);
+                finish();
+                break;
+        }
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return(super.onOptionsItemSelected(item));
+    }
+    @Override
+    public void onBackPressed() {
+        NavUtils.navigateUpFromSameTask(com.savaari.savaari_driver.register.RegisterActivity.this);
+    }
+}
---
+++ Saavari_Driver/app/src/main/java/com/savaari/savaari_driver/register/RegisterViewModel.java
@@ -0,0 +1,87 @@
+package com.savaari.savaari_driver.register;
+
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.savaari.savaari_driver.Repository;
+import com.savaari.savaari_driver.entity.Driver;
+
+public class RegisterViewModel extends ViewModel
+{
+    // ---------------------------------------------
+    //              Main Attributes
+    // ---------------------------------------------
+
+    // Data Attributes
+    private final Repository repository;
+    private Driver driver;
+
+    // Logging Tags
+    private final String TAG = this.getClass().getCanonicalName();
+
+    // Flags
+    private final MutableLiveData<Boolean> userdataLoaded = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> persistConnection = new MutableLiveData<>();
+
+    // -------------------------------------------------
+    //              Main Methods
+    // -------------------------------------------------
+
+    // Main Constructor
+    public RegisterViewModel(Repository repository) {
+        this.repository = repository;
+        driver = repository.getDriver();
+        if (driver != null) {
+            userdataLoaded.setValue(true);
+        } else {
+            userdataLoaded.setValue(false);
+        }
+    }
+
+    // Getters and Setters
+    public Driver getDriver() {
+        return driver;
+    }
+    public LiveData<Boolean> getUserdataLoaded() {
+        return userdataLoaded;
+    }
+    public LiveData<Boolean> getPersistConnection() {
+        return persistConnection;
+    }
+    // --------------------------------------------------
+    //                  Main Methods
+    // --------------------------------------------------
+
+    // Persisting Connection
+    public void persistConnection(int userID) {
+        repository.persistLogin(object -> {
+            if (object != null) {
+                persistConnection.postValue((boolean) object);
+            } else {
+                persistConnection.postValue(false);
+            }
+        }, userID);
+    } // end of function
+
+    // Loading User Data
+    public void loadUserData(int userID) {
+        repository.loadUserData(object -> {
+            try {
+                if (object != null) {
+                    repository.setDriver((Driver) object);
+                    driver = repository.getDriver();
+                    Log.d(TAG, "loadDriverData: User Data Loaded!");
+                    userdataLoaded.postValue(true);
+                } else {
+                    userdataLoaded.postValue(false);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                userdataLoaded.postValue(false);
+            }
+        }, userID);
+    } // End of function
+}
