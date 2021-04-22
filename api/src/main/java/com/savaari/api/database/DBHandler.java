8d1438cd675d62974b7f8821b87bc0467b3dd076
Farjad Ilyas <ilyasfarjad@gmail.com>
2021-04-22 06:03:04 +0500

    Add Admin, Driver, Vehicle classes & define DB ops
    - DBHandler defines Driver & Admin Login interface
    - DBHandler defines Registration Request submission & response interface
    - config.properties defines which DBHandler implementation to use
    - Argon2 Hashing for Passwords

--- api/src/main/java/com/savaari/api/database/DBHandler.java
+++ api/src/main/java/com/savaari/api/database/DBHandler.java
@@ -1,4 +1,35 @@
 package com.savaari.api.database;
 
+import com.savaari.api.entity.Administrator;
+import com.savaari.api.entity.Driver;
+import com.savaari.api.entity.Location;
+import com.savaari.api.entity.Vehicle;
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
 public interface DBHandler {
+
+    /* CRUD Methods*/
+    Boolean addDriver(String username, String emailAddress, String password);
+    int loginDriver(Driver driver);
+    boolean fetchDriverData(Driver driver);
+
+    /* Registration Methods */
+    boolean sendRegistrationRequest(Driver driver);
+
+    /*Driver-Vehicle methods*/
+    boolean sendVehicleRegistrationRequest(Driver driver, Vehicle currentVehicleRequest);
+    boolean respondToVehicleRegistrationRequest(Vehicle currentVehicleRequest);
+
+    /* Unused CRUD methods */
+    JSONArray driverDetails();
+    Boolean deleteDriver();
+
+    boolean respondToDriverRegistrationRequest(Driver driver);
+
+    boolean addAdmin(Administrator admin);
+    boolean loginAdmin(Administrator admin);
+    ArrayList<Vehicle> getVehicleRequests();
+    ArrayList<Driver> getDriverRequests();
 }
---
+++ api/src/main/java/com/savaari/api/entity/Driver.java
@@ -0,0 +1,116 @@
+package com.savaari.api.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.savaari.api.database.DBHandlerFactory;
+
+import java.util.ArrayList;
+
+public class Driver extends User
+{
+	// Status flags values
+	public static final int
+			DV_DEFAULT = 0,
+			DV_REQ_SENT = 1,
+			DV_REQ_REJECTED= 2,
+			DV_REQ_APPROVED = 3;
+
+	// Main Attributes
+	private static final String LOG_TAG = Driver.class.getSimpleName();
+
+	@JsonProperty("CNIC")
+	private String CNIC;
+	private String licenseNumber;
+	private int status;
+	private Vehicle activeVehicle;
+	private ArrayList<Vehicle> vehicles;
+
+	// Main Constructors
+	public Driver() {
+
+	}
+
+	public Driver(Location currentLocation, Boolean active, Boolean takingRide) {
+		super();
+	}
+	
+	// Getters and Setters
+
+	public String getCNIC() {
+		return CNIC;
+	}
+
+	public void setCNIC(String CNIC) {
+		this.CNIC = CNIC;
+	}
+
+	public String getLicenseNumber() {
+		return licenseNumber;
+	}
+
+	public void setLicenseNumber(String licenseNumber) {
+		this.licenseNumber = licenseNumber;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public Vehicle getActiveVehicle() {
+		return activeVehicle;
+	}
+
+	public void setActiveVehicle(Vehicle activeVehicle) {
+		this.activeVehicle = activeVehicle;
+	}
+
+	public void setRegistrationDetails(String firstName, String lastName, String phoneNo, String CNIC, String licenseNumber) {
+		setFirstName(firstName);
+		setLastName(lastName);
+		setPhoneNo(phoneNo);
+		setCNIC(CNIC);
+		setLicenseNumber(licenseNumber);
+	}
+
+	// Main Methods for System Interactions
+
+	// Sign-UP
+
+	// Login
+	public void login()
+	{
+		setUserID(DBHandlerFactory.getInstance().createDBHandler().loginDriver(this));
+	}
+
+	// Fetch Data
+	public boolean fetchData()
+	{
+		return DBHandlerFactory.getInstance().createDBHandler().fetchDriverData(this);
+	}
+
+	public ArrayList<Vehicle> getVehicles() {
+		return vehicles;
+	}
+
+	public void setVehicles(ArrayList<Vehicle> vehicles) {
+		this.vehicles = vehicles;
+	}
+
+	public boolean sendRegistrationRequest() {
+		return DBHandlerFactory.getInstance().createDBHandler().sendRegistrationRequest(this);
+	}
+
+	public boolean sendVehicleRegistrationRequest(Vehicle vehicle) {
+		vehicles.add(vehicle);
+
+		for (Vehicle currentVehicleRequest : vehicles) {
+			DBHandlerFactory.getInstance().createDBHandler().sendVehicleRegistrationRequest(this,
+					currentVehicleRequest);
+			currentVehicleRequest.setStatus(Vehicle.VH_REQ_SENT);
+		}
+		return true;
+	}
+}
