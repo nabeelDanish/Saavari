c50df4f8b211dee20a38b48161550acc4c87924d
Farjad Ilyas <ilyasfarjad@gmail.com>
2021-04-22 08:17:08 +0500

    Server-side: Driver create & log into account
    - CRUDController acts as a facade for all CRUD requests
    - REST Controller extended to include create & log into a Driver account
    - OracleDBHandler implements DB access for these functions

---
+++ api/src/main/java/com/savaari/api/controllers/CRUDController.java
@@ -0,0 +1,58 @@
+package com.savaari.api.controllers;
+
+import com.savaari.api.database.DBHandlerFactory;
+import com.savaari.api.entity.Driver;
+import com.savaari.api.entity.User;
+import com.savaari.api.entity.Vehicle;
+import org.json.JSONObject;
+
+public class CRUDController
+{
+    // Main Attributes
+    private static final String LOG_TAG = CRUDController.class.getSimpleName();
+    Driver driver;
+
+    public CRUDController()
+    {
+
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    /* User CRUD methods */
+
+    // Login Driver
+    public Integer loginDriver(Driver driver) {
+        driver.login();
+        this.driver = driver;
+        return this.driver.getUserID();
+    }
+
+    public void persistDriverLogin(Driver driver) {
+        this.driver = driver;
+    }
+
+    // Add a new inactive Driver method
+    public boolean addDriver(String username, String email_address, String password) {
+        return DBHandlerFactory.getInstance().createDBHandler().addDriver(username, email_address,
+                User.hashPassword(password));
+    }
+
+    public Driver driverData() {
+        if (driver.fetchData()) {
+            return driver;
+        }
+        return null;
+    }
+
+    public JSONObject deleteDriver() {
+        return null;
+    }
+    /* End of section */
+}
--- api/src/main/java/com/savaari/api/database/OracleDBHandler.java
+++ api/src/main/java/com/savaari/api/database/OracleDBHandler.java
@@ -1,11 +1,17 @@
 package com.savaari.api.database;
 
+import com.savaari.api.entity.Driver;
+import com.savaari.api.entity.User;
+import com.savaari.api.entity.Vehicle;
 import org.apache.commons.dbutils.DbUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class OracleDBHandler implements DBHandler {
 
@@ -73,4 +79,181 @@ public class OracleDBHandler implements DBHandler {
             return -1;
         }
     }
+
+    @Override
+    public Boolean addDriver(String username, String emailAddress, String password) {
+
+        return (executeUpdate(String.format("INSERT INTO `DRIVER_DETAILS` (`USER_ID`, `" +
+                        "USER_NAME`, `PASSWORD`, `EMAIL_ADDRESS`) VALUES(%d, '%s', '%s', '%s')",
+                0,
+                username,
+                password,
+                emailAddress))) > 0;
+    }
+
+    @Override
+    public int loginDriver(Driver driver) {
+        Connection connect = null;
+        ResultSet resultSet = null;
+
+        try {
+            String sqlQuery = "SELECT USER_ID, PASSWORD FROM DRIVER_DETAILS WHERE EMAIL_ADDRESS = '" + driver.getEmailAddress() + "'";
+
+            connect = DBCPDataSource.getConnection();
+            resultSet = connect.createStatement().executeQuery(sqlQuery);
+
+            // If email address & password verified
+            if (resultSet.next() && User.verifyPassword(resultSet.getString(2), driver.getPassword())) {
+                System.out.println("Logged in!");
+                return resultSet.getInt("USER_ID");
+            }
+            else {
+                System.out.println("Login failed");
+                return Driver.DEFAULT_ID;
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Exception in DBHandler:loginDriver()");
+            e.printStackTrace();
+            return Driver.DEFAULT_ID;
+        }
+        finally {
+            closeAll(connect, null, resultSet);
+        }
+    }
+
+    @Override
+    public JSONArray driverDetails() {
+        Connection connect = null;
+        ResultSet resultSet = null;
+
+        try {
+            String sqlQuery = "SELECT USER_ID, USER_NAME, PASSWORD, EMAIL_ADDRESS FROM DRIVER_DETAILS";
+
+            connect = DBCPDataSource.getConnection();
+            resultSet = connect.createStatement().executeQuery(sqlQuery);
+
+            JSONArray result = new JSONArray();
+            JSONObject row = new JSONObject();
+
+            while (resultSet.next()) {
+                row.put("USER_ID", resultSet.getInt(1));
+                row.put("USER_NAME", resultSet.getString(2));
+                row.put("PASSWORD", resultSet.getString(3));
+                row.put("EMAIL_ADDRESS", resultSet.getString(4));
+                result.put(row);
+                row = new JSONObject();
+            }
+
+            return result;
+        }
+        catch (Exception e) {
+            System.out.println("Exception in DBHandler:driverDetails()");
+            e.printStackTrace();
+            return null;
+        }
+        finally {
+            closeAll(connect, null, resultSet);
+        }
+    }
+
+    @Override
+    public boolean fetchDriverData(Driver driver) {
+        Connection connect = null;
+        ResultSet resultSet = null;
+
+        try {
+            String sqlQuery = "SELECT USER_NAME, FIRST_NAME, LAST_NAME, PHONE_NO, CNIC, LICENSE_NO, EMAIL_ADDRESS, " +
+                    " STATUS, IS_ACTIVE, ACTIVE_VEHICLE_ID FROM DRIVER_DETAILS WHERE USER_ID = " + driver.getUserID();
+
+            connect = DBCPDataSource.getConnection();
+            resultSet = connect.createStatement().executeQuery(sqlQuery);
+
+            if (resultSet.next()) {
+
+                ArrayList<Vehicle> vehicles = new ArrayList<>();
+                Vehicle currentVehicle;
+
+                driver.setUserID(driver.getUserID());
+                driver.setUsername(resultSet.getString(1));
+                driver.setFirstName(resultSet.getString(2));
+                driver.setLastName(resultSet.getString(3));
+                driver.setPhoneNo(resultSet.getString(4));
+                driver.setCNIC(resultSet.getString(5));
+                driver.setLicenseNumber(resultSet.getString(6));
+                driver.setEmailAddress(resultSet.getString(7));
+                driver.setStatus(resultSet.getInt(8));
+                //driver.setActive(resultSet.getInt(9) == 1);
+
+                int fetchedActiveVehicleID = Integer.parseInt(resultSet.getString(10));
+                if (fetchedActiveVehicleID == Vehicle.DEFAULT_ID) {
+                    driver.setActiveVehicle(null);
+                }
+                else {
+                    driver.setActiveVehicle(new Vehicle(fetchedActiveVehicleID));
+                }
+
+                closeResultSet(resultSet);
+
+                // Retrieve Driver's Vehicles
+                String vehiclesQuery = "SELECT DV.VEHICLE_ID, VT.MAKE, VT.MODEL, VT.YEAR, VT.RIDE_TYPE_ID, DV.NUMBER_PLATE" +
+                        ", DV.STATUS, DV.COLOR" +
+                        " FROM DRIVERS_VEHICLES DV" +
+                        " INNER JOIN VEHICLE_TYPES VT ON DV.VEHICLE_TYPE_ID = VT.VEHICLE_TYPE_ID" +
+                        " WHERE DV.DRIVER_ID = " + driver.getUserID();
+
+                resultSet = connect.createStatement().executeQuery(vehiclesQuery);
+
+                while (resultSet.next()) {
+                    currentVehicle = new Vehicle(resultSet.getInt(1), resultSet.getString(2),
+                            resultSet.getString(3), resultSet.getString(4), resultSet.getInt(5),
+                            resultSet.getString(6), resultSet.getInt(7),
+                            resultSet.getString(8));
+                    vehicles.add(currentVehicle);
+
+                    if (driver.getActiveVehicle() == null && currentVehicle.getStatus() == Vehicle.VH_ACCEPTANCE_ACK)  {
+                        driver.setActiveVehicle(new Vehicle(currentVehicle.getStatus()));
+                    }
+                }
+
+                closeResultSet(resultSet);
+
+                // Retrieve Driver's Vehicle requests
+                String vehicleRequestQuery = "SELECT REGISTRATION_REQ_ID, MAKE, MODEL, YEAR, NUMBER_PLATE, STATUS, COLOR" +
+                        " FROM VEHICLE_REGISTRATION_REQ" +
+                        " WHERE DRIVER_ID = " + driver.getUserID();
+
+                resultSet = connect.createStatement().executeQuery(vehicleRequestQuery);
+
+                while (resultSet.next()) {
+                    currentVehicle = new Vehicle(resultSet.getInt(1), resultSet.getString(2),
+                            resultSet.getString(3), resultSet.getString(4), 0,
+                            resultSet.getString(5), resultSet.getInt(6),
+                            resultSet.getString(7));
+                    vehicles.add(currentVehicle);
+                }
+
+                driver.setVehicles(vehicles);
+
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Exception in DBHandler:driverData()");
+            e.printStackTrace();
+            return false;
+        }
+        finally {
+            closeAll(connect, null, resultSet);
+        }
+    }
+
+    @Override
+    public Boolean deleteDriver() {
+        return null;
+    }
+    /* End of section */
 }
