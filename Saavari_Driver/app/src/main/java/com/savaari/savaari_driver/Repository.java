46c4fef436674c4d09ec1feb51b9466ec4d26446
Nabeel Danish <nabeelben@gmail.com>
2021-05-22 13:35:41 +0500

    Driver App: RideRequest Processing
    - Added RideRequest entity class
    - Added Rider entity class
    - Added Repository and network calls for API

--- Saavari_Driver/app/src/main/java/com/savaari/savaari_driver/Repository.java
+++ Saavari_Driver/app/src/main/java/com/savaari/savaari_driver/Repository.java
@@ -3,7 +3,6 @@ package com.savaari.savaari_driver;
 import com.savaari.savaari_driver.entity.*;
 import com.savaari.savaari_driver.services.network.NetworkUtil;
 import com.savaari.savaari_driver.services.network.OnDataLoadedListener;
-
 import java.util.concurrent.Executor;
 
 public class Repository
@@ -41,21 +40,93 @@ public class Repository
     public void loadUserData(OnDataLoadedListener callback, int currentUserID) {
         executor.execute(() -> callback.onDataLoaded(NetworkUtil.getInstance().loadUserData(currentUserID)));
     }
-    // Sending Register Driver request
-    public void sendRegisterDriverRequest(OnDataLoadedListener callback, Driver driver) {
-        executor.execute(() -> callback.onDataLoaded(NetworkUtil.getInstance().sendRegistrationRequest(driver)));
+
+    // Get User Locations
+    public void getUserLocations(OnDataLoadedListener callback) {
+        executor.execute(() -> callback.onDataLoaded(NetworkUtil.getInstance().getUserLocations()));
     }
-    // Sending Vehicle Registration Request
-    public void sendVehicleRegistrationRequest(OnDataLoadedListener callback, Driver driver, Vehicle vehicle)
-    {
-        executor.execute(() -> callback.onDataLoaded(NetworkUtil.getInstance().sendVehicleRegistrationRequest(driver, vehicle)));
-    }
+
     // Send Last Location
     public void sendLastLocation(OnDataLoadedListener callback, int currentUserID, double latitude, double longitude) {
         executor.execute(() ->
                 callback.onDataLoaded(NetworkUtil.getInstance().sendLastLocation(currentUserID, latitude, longitude)));
     }
 
+    // Set Mark Active
+    public void setMarkActive(OnDataLoadedListener callback, int userID, int active_status)
+    {
+        executor.execute(() ->
+                callback.onDataLoaded(NetworkUtil.getInstance().setMarkActive(userID, active_status)));
+    }
+
+    // Check Ride Request Status
+    public void checkRideRequestStatus(OnDataLoadedListener callback, int userID) {
+        executor.execute(() -> callback.onDataLoaded(NetworkUtil.getInstance().checkRideRequestStatus(userID)));
+    }
+
+    // Start Matchmaking Service
+    public void startMatchmaking(OnDataLoadedListener callback, int userID) {
+        executor.execute(() -> callback.onDataLoaded(NetworkUtil.getInstance().startMatchmaking(userID)));
+    }
+
+    // Check ride status
+    public void checkRideStatus(OnDataLoadedListener callback, int userID, int riderID, int rideTypeID)
+    {
+        executor.execute(() -> callback.onDataLoaded(NetworkUtil.getInstance().checkRideStatus(userID, riderID, rideTypeID)));
+    }
+
+    // Confirm Ride Request
+    public void confirmRideRequest(OnDataLoadedListener callback, int userID, int found_status, int riderID)
+    {
+        executor.execute(() -> callback.onDataLoaded(NetworkUtil.getInstance().confirmRideRequest(userID, found_status, riderID)));
+    }
+
+    // Marking Arrival
+    public void markArrival(OnDataLoadedListener callback, int rideID)
+    {
+        executor.execute(() -> callback.onDataLoaded(NetworkUtil.getInstance().markArrival(rideID)));
+    }
+
+    // Starting Ride
+    public void startRide(OnDataLoadedListener callback, int rideID)
+    {
+        executor.execute(() -> callback.onDataLoaded(NetworkUtil.getInstance().startRide(rideID)));
+    }
+
+    // Marking Arrival at destination
+    public void markDriverAtDestination(OnDataLoadedListener callback, Ride ride, double dist_travelled, int driverID)
+    {
+        executor.execute(() -> callback.onDataLoaded(NetworkUtil.getInstance().markDriverAtDestination(ride, dist_travelled, driverID)));
+    }
+
+    // Ending Ride with Payment
+    public void endRideWithPayment(OnDataLoadedListener callback, int rideID, Payment payment, int driverID)
+    {
+        executor.execute(() -> callback.onDataLoaded(NetworkUtil.getInstance().endRideWithPayment(rideID, payment, driverID)));
+    }
+
+    // Sending Register Driver request
+    public void sendRegisterDriverRequest(OnDataLoadedListener callback, Driver driver) {
+        executor.execute(() -> callback.onDataLoaded(NetworkUtil.getInstance().sendRegistrationRequest(driver)));
+    }
+
+    // Sending Vehicle Registration Request
+    public void sendVehicleRegistrationRequest(OnDataLoadedListener callback, Driver driver, Vehicle vehicle)
+    {
+        executor.execute(() -> callback.onDataLoaded(NetworkUtil.getInstance().sendVehicleRegistrationRequest(driver, vehicle)));
+    }
+
+    // Select Active Vehicle
+    public void selectActiveVehicle(OnDataLoadedListener callback, int driverID, int vehicleID)
+    {
+        executor.execute(() -> callback.onDataLoaded(NetworkUtil.getInstance().selectActiveVehicle(driverID, vehicleID)));
+    }
+
+    // Give Feedback for Rider
+    public void giveFeedbackForRider(OnDataLoadedListener callback, Ride ride, float rating)
+    {
+        executor.execute(() -> callback.onDataLoaded(NetworkUtil.getInstance().giveFeedbackForRider(ride, rating)));
+    }
 
     // Getters and Setters
     public Driver getDriver() {
---
+++ Saavari_Driver/app/src/main/java/com/savaari/savaari_driver/entity/RideRequest.java
@@ -0,0 +1,114 @@
+package com.savaari.savaari_driver.entity;
+
+public class RideRequest {
+
+    // Flags
+    // Match status - used in business logic
+    public static final int
+            MS_DEFAULT = 0,
+            MS_REQ_RECEIVED = 1,
+            MS_REQ_ACCEPTED = 2;
+
+    // Match status - used in business logic
+    public static final int
+            NOT_SENT = -1,
+            NO_CHANGE = 0,
+            REJECTED = 1,
+            FOUND = 2;
+
+    // Match return status  - returned to clients
+    public static final int
+            DEFAULT = 50,
+            PAIRED = 51,
+            ALREADY_PAIRED = 52,
+            NOT_PAIRED = 53,
+            NOT_FOUND = 54,
+            STATUS_ERROR = 55;
+    // ----------------------------------------------------------------
+    //                         MAIN ATTRIBUTES
+    // ----------------------------------------------------------------
+    protected Rider rider;
+    protected Driver driver;
+    protected Location pickupLocation;
+    private String pickupTitle;
+    protected Location dropoffLocation;
+    private String dropoffTitle;
+    protected int findStatus;
+    protected int paymentMethod;
+    private RideType rideType;
+    private boolean splittingFare;
+
+    // ----------------------------------------------------------------
+    //                          MAIN METHODS
+    // ----------------------------------------------------------------
+
+    // Default Constructor
+    public RideRequest() {
+        // Empty Constructor
+        driver = new Driver();
+        rider = new Rider();
+        rideType = new RideType();
+    }
+
+    // Getters and Setters
+    public Rider getRider() {
+        return rider;
+    }
+    public void setRider(Rider rider) {
+        this.rider = rider;
+    }
+    public Driver getDriver() {
+        return driver;
+    }
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+    public Location getPickupLocation() {
+        return pickupLocation;
+    }
+    public void setPickupLocation(Location pickupLocation) {
+        this.pickupLocation = pickupLocation;
+    }
+    public Location getDropoffLocation() {
+        return dropoffLocation;
+    }
+    public void setDropoffLocation(Location dropoffLocation) {
+        this.dropoffLocation = dropoffLocation;
+    }
+    public String getPickupTitle() {
+        return pickupTitle;
+    }
+    public void setPickupTitle(String pickupTitle) {
+        this.pickupTitle = pickupTitle;
+    }
+    public String getDropoffTitle() {
+        return dropoffTitle;
+    }
+    public void setDropoffTitle(String dropoffTitle) {
+        this.dropoffTitle = dropoffTitle;
+    }
+    public int getFindStatus() {
+        return findStatus;
+    }
+    public void setFindStatus(int findStatus) {
+        this.findStatus = findStatus;
+    }
+    public Integer getPaymentMethod() {
+        return paymentMethod;
+    }
+    public void setPaymentMethod(Integer paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+    public boolean isSplittingFare() {
+        return splittingFare;
+    }
+    public void setSplittingFare(boolean splittingFare) {
+        this.splittingFare = splittingFare;
+    }
+    public RideType getRideType() {
+        return rideType;
+    }
+    public void setRideType(RideType rideType) {
+        this.rideType = rideType;
+    }
+}
