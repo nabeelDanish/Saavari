909f42413b9416aea0263d8815e95ddd70002f1f
Farjad Ilyas <ilyasfarjad@gmail.com>
2021-05-22 19:59:53 +0400

    Rider App: Ride config RecyclerView Adapters
    - Add RecyclerView Adapter for RideType selection
    - Add RecyclerView Adapter for Payment Method selection

---
+++ Savaari_Rider/app/src/main/java/com/savaari/savaari_rider/ride/adapter/ItemClickListener.java
@@ -0,0 +1,6 @@
+package com.savaari.savaari_rider.ride.adapter;
+
+public interface ItemClickListener {
+    void onRideTypeItemClick(int position);
+    void onPaymentMethodItemClick(int position);
+}
