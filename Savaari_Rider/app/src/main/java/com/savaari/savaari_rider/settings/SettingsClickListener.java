81978977aff0732d4c77c5fa27895127cfb01c11
Farjad Ilyas <ilyasfarjad@gmail.com>
2021-05-22 19:13:55 +0400

    Rider App: Added App Settings flow
    - SettingsActivity implementation
    - Uses fragments for sub-settings
    - Dynamic theme-change feature

---
+++ Savaari_Rider/app/src/main/java/com/savaari/savaari_rider/settings/SettingsClickListener.java
@@ -0,0 +1,12 @@
+package com.savaari.savaari_rider.settings;
+
+public interface SettingsClickListener {
+    void onAccountClicked();
+    void onGeneralClicked();
+    void onThemeClicked();
+    void onProductivityClicked();
+    void onRemindersClicked();
+    void onNotificationsClicked();
+    void onSupportClicked();
+    void onLogoutClicked();
+}
---
+++ Savaari_Rider/app/src/main/java/com/savaari/savaari_rider/settings/SettingsViewModel.java
@@ -0,0 +1,10 @@
+package com.savaari.savaari_rider.settings;
+
+import androidx.lifecycle.ViewModel;
+
+public class SettingsViewModel extends ViewModel {
+
+    public void logout() {
+
+    }
+}
